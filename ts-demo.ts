880f7f7754ad06a7f29016fb426ea4f4f0bc7ac3
Samekshya <samikshyabaniya003@gmail.com>
2025-11-20 07:23:17 +0545

    first commit

---
+++ ts-demo.ts
@@ -0,0 +1,153 @@
+let message: string = "Hello";
+console.log(message);
+//message = 0 //error
+
+//Primitive types
+let booleanVar: boolean = true;
+let numberVar: number = 42;
+let symbolVar:symbol = Symbol("unqiue");
+let nullVar: null = null;
+let undefinedVar: undefined = undefined;
+
+let anyVar: any = "Could be anything";
+anyVar = anyVar + 100; //No error
+
+let unknownVar: unknown = "Could be anything too";
+//unknownVar = unknownVar + 100; //Error
+//unknown is type assertion
+
+let scores: number[] = [90,80,70];
+console.log(scores)
+//tuples
+let tupleVar: [string, number] = ["Alice", 30];
+console.log(tupleVar)
+//tupleVar = [30, "Alice"] //Error
+
+//Union
+let id: string | number | boolean;
+id = "Hello";
+console.log(id)
+id = 123;
+console.log(id);
+//id = true //error
+
+//functions
+function add(num1:number, num2: number): string{
+    let sum: number = num1 + num2;
+    return `Sum is ${sum}`;
+}
+let result: String = add(10,20);
+console.log(result)
+
+const greet = (name: string = "Guest"): void =>{
+    console.log(`Hello, ${name}`);
+}
+greet(); //name is optiona; if default value
+
+//Object Defination
+//1.Object Literal
+let person: {name: string; age: number } = {
+    name: "Bob",
+    age: 25,
+    //isActive: true //error
+};
+console.log(person, person.name);
+
+//2.Interface
+interface Product{
+    id:number;
+    name: string;
+    price: number;
+    description?: string; //optional
+}
+const laptop: Product = {
+    id: 1,
+    name: "Laptop",
+    price:1500
+};
+console.log(laptop);
+
+type Student = {
+    id: number;
+    name: string;
+    grade: string;
+    product?:Product
+}
+let student1: Student = {
+    id: 101,
+    name: "Charlie",
+    grade: "A",
+    product: laptop
+};
+console.log(student1);
+
+//Generics
+function identity<T>(arg: T): T {
+    return arg;
+}
+//Data type injected at call time
+let output1: string = identity<string>("myString");
+console.log(output1);
+let output2: number = identity<number>(100);
+console.log(output2);
+
+//enum
+//named constant
+enum Role{
+    Admin, //Admin = 0
+    USer,
+    Guest
+}
+let userRole: Role = Role.Admin;
+console.log(userRole); //0
+console.log(userRole == Role.Admin) //constant check
+
+let userRoleName: string = "admin";
+console.log(userRoleName == "Admin") //case sensitive check
+
+//Generic Usercase
+interface User{
+    id:number,
+    name: string,
+    role: Role;
+}
+let optUser: Partial <User> = {
+    name: "Dave"
+};
+//every attribute is optional
+console.log(optUser);
+let readonlyUser: Readonly<User> = {
+    id: 1 ,
+    name: "Eve",
+    role: Role.USer
+};
+//readonlyUser.id = 2; //Error
+console.log(readonlyUser);
+
+//Task
+//Create enum for CarType: Sedan, SUv, Truck, Coupe.
+// create a type CarModel
+// -name : string, description: string
+// Create an interface for a Car with properties:
+// make: string or number, model: CarModel;
+// year: number,
+// type: CarType, and isElectric (optional) boolena or number or string.
+// create a array of cars with atleast 3 car objects.
+// filter the cars whose year is greater than 2015
+enum CarType{
+    Sedan,
+    SUV,
+    Truck,
+    Coupe
+}
+type CarModel{
+    name: String,
+    descriptional: String
+}
+interface Car{
+    make: string | number;
+    model: CarModel;
+    year: number;
+    type: CarType;
+    isElectric?: boolean | number | string;
+}
